c42fa44155f38f5fc1aea291a5156c069b29fa4c
CX61 <CX61@Hwang>
2015-03-26 09:05:36 +0900

    과제 제출합니다

--- javapractice/src/org/dimigo/basic/Condition.java
+++ javapractice/src/org/dimigo/basic/Condition.java
@@ -4,14 +4,14 @@ public class Condition {
 	public static void main(String[] args) {
 		System.out.println("<< 고속도로 통행료 계산>>");
 		int distance = 25;
-		String vehi = "고속버스";
+		String vehi = "그 외";
 		int price;
 		switch(vehi)
 		{
 		case "경차":
-			if(distance >= 10)
+			if(distance > 10)
 			{
-				if(distance >= 20)
+				if(distance > 20)
 				{
 					price = 700;
 				}
@@ -21,9 +21,9 @@ public class Condition {
 			else price = 300;
 			break;
 		case "고속버스":
-			if(distance >= 10)
+			if(distance > 10)
 			{
-				if(distance >= 20)
+				if(distance > 20)
 				{
 					price = 1450;
 				}
@@ -33,9 +33,9 @@ public class Condition {
 			else price = 850;
 			break;
 		default:
-			if(distance >= 10)
+			if(distance > 10)
 			{
-				if(distance >= 20)
+				if(distance > 20)
 				{
 					price = 1000;
 				}
