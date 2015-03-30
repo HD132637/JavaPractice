663db6f095e0f68b05ae5bc774252f26c4d1a68a
CX61 <CX61@Hwang>
2015-03-30 15:59:24 +0900

    수정했습니다

--- javapractice/src/org/dimigo/basic/Loop.java
+++ javapractice/src/org/dimigo/basic/Loop.java
@@ -2,17 +2,15 @@ package org.dimigo.basic;
 
 public class Loop {
 	public static void main(String[] args) {
-		for(int i=1; i<=10; i++)
-		{
-			for(int j=1; j<=10; j++)
-			{
-				if(i==j) {
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= 10; j++) {
+				if (i == j) {
 					System.out.printf("*");
 					continue;
-					}
+				}
 				System.out.print(j);
 			}
-			System.out.println( );
+			System.out.println();
 		}
 	}
 
