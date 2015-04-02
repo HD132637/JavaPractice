e954c9bbc9c93f97ecc7774fe44899efca7837a4
CX61 <CX61@Hwang>
2015-04-02 09:32:52 +0900

    ..

--- JavaPractice/src/org/dimigo/basic/Loop.java
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -1,5 +1,19 @@
 package org.dimigo.basic;
 
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ * 		|_ Loop
+ *  
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 4. 2.
+ * </pre>
+ *
+ * @author CX61
+ * @version 1.0.0
+ * 
+ */
 public class Loop {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
