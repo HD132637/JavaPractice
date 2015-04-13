423284a1102d32d4bb05b502f3e1de743e951af3
CX61 <CX61@Hwang>
2015-04-13 16:23:00 +0900

    과제 제출합니다.

---
+++ JavaPractice/src/org/dimigo/oop/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * <pre>
+ * org.dimigo.oop
+ * 		|_ package-info
+ *  
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 4. 13.
+ * </pre>
+ *
+ * @author CX61
+ * @version 1.0.0
+ * 
+ */
+package org.dimigo.oop;
